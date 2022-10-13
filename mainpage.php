a57f8a054319d4aad930f67e95a0ba2c5fce56f9
Lukada-taiya <lukmanadam43@gmail.com>
2022-10-13 21:23:12 +0000

    Added frontend to app. Completed navbar of site

---
+++ mainpage.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">   	
+		<link rel="stylesheet" href="css/bootstrap.min.css"/>			
+		<link rel="stylesheet" href="css/all.min.css"/>	
+		<link rel="stylesheet" href="css/frontstyles.css"/>		
+		<script src="js/jquery-3.6.0.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script> 
+		<script src="js/all.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<title>Mind Blog</title>
+	</head>
+	<body>
+		<div class="nav-element">
+			<div style="background-color: #27aae1; height: 10px;" role="navigation">		
+			</div>
+				<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+				  <div class="container-fluid">
+					 <a class="navbar-brand" href="mainpage.php"><span class="h4">MindBlog</span></a>
+					 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					 </button>
+					 <div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+						  <li class="nav-item">
+							 <a class="nav-link active" aria-current="page" href="mainpage.php">Home</a>
+						  </li>
+						  <li class="nav-item">
+							 <a class="nav-link" href="#">Blog</a>
+						  </li>  
+						  <li class="nav-item">
+							 <a class="nav-link" href="#">About Us</a>
+						  </li>  
+						  <li class="nav-item">
+							 <a class="nav-link" href="#">Services</a>
+						  </li>  
+						  <li class="nav-item">
+							 <a class="nav-link" href="#">Contact Us</a>
+						  </li>  
+						  <li class="nav-item">
+							 <a class="nav-link" href="#">Features</a>
+						  </li>  
+						</ul>
+						<form class="d-flex">
+						  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+						  <button class="btn btn-outline-primary" type="submit">Search</button>
+						</form>
+					 </div>
+			  </div>			  	
+			</nav>	
+			<div class="blue-band" style="background-color: #27aae1; height: 10px;" role="navigation"/>
+		</div>
+		<div class="container">
+			<div class="blog-header">
+				<h1 class="h1">New Blog Post</h1>
+				<p class="lead">This is the sub text</p> 
+			</div>
+			<div class="row">
+				<div class="col-md-8">
+					<p>Documentation and examples for Bootstrap’s powerful, responsive navigation header, the navbar. Includes support for branding, navigation, and more, including support for our collapse plugin.
+					Documentation and examples for Bootstrap’s powerful, responsive navigation header, the navbar. Includes support for branding, navigation, and more, including support for our collapse plugin.
+					Documentation and examples for Bootstrap’s powerful, responsive navigation header, the navbar. Includes support for branding, navigation, and more, including support for our collapse plugin.
+					Documentation and examples for Bootstrap’s powerful, responsive navigation header, the navbar. Includes support for branding, navigation, and more, including support for our collapse plugin.</p>
+				</div>
+				<div class="col-md-3 offset-1">
+					Documentation and examples for Bootstrap’s powerful, responsive navigation header, the navbar. Includes support for branding, navigation, and more, including support for our collapse plugin.Documentation and examples for Bootstrap’s powerful, responsive navigation header, the navbar. Includes support for branding, navigation, and more, including support for our collapse plugin.Documentation and examples for Bootstrap’s powerful, responsive navigation header, the navbar. Includes support for branding, navigation, and more, including support for our collapse plugin.Documentation and examples for Bootstrap’s powerful, responsive navigation header, the navbar. Includes support for branding, navigation, and more, including support for our collapse plugin.
+				</div>
+			</div>
+		
+		</div>
+		<footer>
+			<div class="footer-content">
+				<p>MindBlog Ltd &copy; All Rights Reserved </p>
+			</div>
+		</footer>	
+		<div style="background-color: #27aae1; height: 10px;" role="navigation"/>
+	</body>
+</html>
