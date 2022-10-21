da1452a9a558e9bb0a8fba8074575e1b941a6917
Lukada-taiya <lukmanadam43@gmail.com>
2022-10-21 21:25:52 +0000

    Added table to show all posts on admin dashboard.

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,6 @@
 <?php require_once('include/session.php') ?>
 <?php require_once('include/functions.php') ?>
+<?php require_once('include/db.php') ?>
 <!doctype html>
 <html>
 	<head>
@@ -73,45 +74,75 @@
 					</ul> 
 				</div>
 				<div class="col-md-10">
-					<h4 class="mt-2">About</h4>
 					<div>
 						<?php 
 							echo error_message(); 
 							echo success_message(); 					
-						?>
+						?>	
 					</div>
-					<p> This is just a random paragraph. I hope you are doing remarkably well as
-					a dummy paragraph. So I will sign out now. Goodbye</p>
-					<p> This is just a random paragraph. I hope you are doing remarkably well as
-					a dummy paragraph. So I will sign out now. Goodbye</p>
-					<p> This is just a random paragraph. I hope you are doing remarkably well as
-					a dummy paragraph. So I will sign out now. Goodbye</p>
-					<p> This is just a random paragraph. I hope you are doing remarkably well as
-					a dummy paragraph. So I will sign out now. Goodbye</p>
-					<p> This is just a random paragraph. I hope you are doing remarkably well as
-					a dummy paragraph. So I will sign out now. Goodbye</p>
-					<p> This is just a random paragraph. I hope you are doing remarkably well as
-					a dummy paragraph. So I will sign out now. Goodbye</p>
-					<p> This is just a random paragraph. I hope you are doing remarkably well as
-					a dummy paragraph. So I will sign out now. Goodbye</p>
-					<p> This is just a random paragraph. I hope you are doing remarkably well as
-					a dummy paragraph. So I will sign out now. Goodbye</p>
-					<p> This is just a random paragraph. I hope you are doing remarkably well as
-					a dummy paragraph. So I will sign out now. Goodbye</p>
-					<p> This is just a random paragraph. I hope you are doing remarkably well as
-					a dummy paragraph. So I will sign out now. Goodbye</p>
-					<p> This is just a random paragraph. I hope you are doing remarkably well as
-					a dummy paragraph. So I will sign out now. Goodbye</p>
-					<p> This is just a random paragraph. I hope you are doing remarkably well as
-					a dummy paragraph. So I will sign out now. Goodbye</p>
-					<p> This is just a random paragraph. I hope you are doing remarkably well as
-					a dummy paragraph. So I will sign out now. Goodbye</p>
-					<p> This is just a random paragraph. I hope you are doing remarkably well as
-					a dummy paragraph. So I will sign out now. Goodbye</p>
-					<p> This is just a random paragraph. I hope you are doing remarkably well as
-					a dummy paragraph. So I will sign out now. Goodbye</p>
-					<p> This is just a random paragraph. I hope you are doing remarkably well as
-					a dummy paragraph. So I will sign out now. Goodbye</p>					
+					<div>
+						<h1>Admin Dashboard</h1>
+					</div>
+					<div class="table-responsive">
+						<table class="table">
+						<thead>
+							<tr>
+								<th>No.</th>
+								<th>Title</th>
+								<th>Date</th>
+								<th>Author</th>
+								<th>Category</th>
+								<th>Banner</th>
+								<th>Comment</th>
+								<th>Action</th>
+								<th>Details</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php	
+							global $connection; 
+							$query = "SELECT post.id,post.title,post.body,post.image,post.datetime,category.name, admin.name AS 
+								author 
+								FROM post
+								INNER JOIN category ON post.category_id = category.id
+								INNER JOIN admin ON post.admin_id = admin.id
+								ORDER BY post.datetime DESC";
+							$execute = mysqli_query($connection,$query);
+							$sId = 0;
+							while($data = mysqli_fetch_array($execute)) { 
+								$id = $data['id'];
+								$title = $data['title']; 
+								$image = $data['image'];
+								$category = $data['name'];
+								$author = $data['author']; 
+								$datetime = $data['datetime'];
+								$sId++;
+							?> 
+							<tr>
+								<td><?php echo $sId ?></td>
+								<td title="<?php echo $title ?>"><?php echo strlen($title) > 17 ? substr($title,0,17)."...":
+								$title ?></td>
+								<td title="<?php echo $datetime ?>"><?php echo (strlen($datetime) > 17) ? substr($datetime,0,17)."...":
+								$datetime ?></td>
+								<td title="<?php echo $author ?>"><?php echo strlen($author) > 17 ? substr($author,0,17)."...":
+								$author ?></td>
+								<td title="<?php echo $category ?>"><?php echo strlen($category) > 17 ? substr($category,0,17)."...":
+								$category ?></td>
+								<td><img src="img/post/<?php echo $image ?>" width='170px' height='50px'/></td>
+								<td><?php echo "Comment" ?></td>
+								<td class="wide-cell">
+									<a href='editpost.php' class='btn btn-warning btn-sm'>Edit</a>
+									<a href='deletepost.php' class='btn btn-danger btn-sm'>Delete</a>
+								</td>
+								<td class="wide-cell">
+									<a href="singlepost.php?id=<?php echo $id ?>" target="_blank" class='btn btn-success btn-sm'>Live 
+									Preview</a>
+								</td>														
+							</tr>
+							<?php } ?>
+							</tbody>
+						</table>
+					</div>
 				</div>
 			</div>
 		</div>
